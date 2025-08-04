ecba31fcc17dfb0ce25667173d7dd0c965808515
Luanna Fernanda <annalauracodes@gmail.com>
2025-08-04 17:37:40 -0300

    primeiro commit

---
+++ src/services/cart.js
@@ -0,0 +1,62 @@
+//quais açoes meu carrinho pode fazer
+
+
+async function addItem(userCart, item) {
+  userCart.push(item);
+}
+
+
+
+async function calculateTotal(userCart) {
+  console.log("\nShopee Cart TOTAL IS:");
+
+  const result = userCart.reduce((total, item) => total + item.subtotal(), 0);
+  console.log(`🎁Total: ${result}`);
+}
+
+
+async function deleteItem(userCart, name) {
+  const index = userCart.findIndex((item) => item.name === name);
+
+  if (index !== -1) {
+    userCart.splice(index, 1);
+  }
+}
+
+
+async function removeItem(userCart, item) {
+  //1. encontrar o indice do item
+  const indexFound = userCart.findIndex((p) => p.name === item.name);
+
+  //2. Caso não encontre o item
+  if (indexFound == -1) {
+    console.log("item não encontrado");
+    return;
+  }
+
+  //3. item > 1 subtrair um item
+  if (userCart[indexFound].quantity > 1) {
+    userCart[indexFound].quantity -= 1;
+    return;
+  }
+
+  //4. caso item = 1 deletar o item
+  if (userCart[indexFound].quantity == 1) {
+    userCart.splice(indexFound, 1);
+    return;
+  }
+}
+
+// mostra todos os items do carrinho
+async function displaycart(userCart) {
+  console.log("\nShopee cart list:");
+  userCart.forEach((item, index) => {
+    console.log(
+      `${index + 1}. ${item.name} - R$ ${item.price} | ${
+        item.quantity
+      }x | Subtotal = ${item.subtotal()}`
+    );
+  });
+}
+
+export { addItem, calculateTotal, deleteItem, removeItem, displaycart };
